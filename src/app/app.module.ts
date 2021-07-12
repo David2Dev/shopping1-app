00b19643f89a2e84616d5533dfe2d605a569839b
David Whitehead <david_whitehead_234@yahoo.com>
2021-07-12 13:36:38 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+//import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BannerComponent } from './banner/banner.component';
+import { CategoriesimgComponent } from './categoriesimg/categoriesimg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    CategoriesimgComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+   // AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
